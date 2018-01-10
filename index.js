2467f7a01876c078afcbf339270f00fd7b31a1b8
Anirudh <anirudhbs96@gmail.com>
2018-01-10 11:13:09 +0530

    Add routes to fetch projects of a student

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/students', (req, res) => {
-  const queryString = 'SELECT * FROM students WHERE status=TRUE'
+  const queryString = 'SELECT id, name FROM students WHERE status=TRUE'
   client.query(queryString, (err, response) => {
     if (err) throw err
     else {
@@ -32,38 +32,74 @@ app.post('/students', (req, res) => {
   })
 })
 
-app.post('/student', (req, res) => {
-  const queryString = 'INSERT INTO students VALUES($1, $2, $3) RETURNING id'
-  const values = [req.body.id, req.body.name, true]
+app.post('/:studentid/projects', (req, res) => {
+  const id = req.params.studentid
+  const queryString = 'SELECT project_id, user_id, repository FROM projects WHERE user_id = $1'
+  const values = [id]
   client.query(queryString, values, (err, response) => {
     if (err) throw err
     else {
-      res.send(response.rows[0])
+      res.send(response.rows)
     }
   })
 })
 
-app.delete('/student', (req, res) => {
-  const queryString = 'DELETE FROM students WHERE id= $1 RETURNING id'
-  const values = [req.body.id]
+app.post('/:studentid/demos', (req, res) => {
+  const id = req.params.studentid
+  const queryString = 'SELECT user_id, project_id, rating, date FROM demos WHERE user_id = $1'
+  const values = [id]
   client.query(queryString, values, (err, response) => {
     if (err) throw err
     else {
-      res.send(response.rows[0])
+      res.send(response.rows)
     }
   })
 })
 
-app.put('/student', (req, res) => {
-  const queryString = 'UPDATE students SET name=$2 WHERE id=$1 RETURNING id'
-  const values = [req.body.id, req.body.name]
+app.post('/:studentid/attendance', (req, res) => {
+  const id = req.params.studentid
+  const queryString = 'SELECT user_id, date_absent, reason FROM attendance WHERE user_id = $1'
+  const values = [id]
   client.query(queryString, values, (err, response) => {
     if (err) throw err
     else {
-      res.send(response.rows[0])
+      res.send(response.rows)
     }
   })
 })
+
+// app.post('/student', (req, res) => {
+//   const queryString = 'INSERT INTO students VALUES($1, $2, $3) RETURNING id'
+//   const values = [req.body.id, req.body.name, true]
+//   client.query(queryString, values, (err, response) => {
+//     if (err) throw err
+//     else {
+//       res.send(response.rows[0])
+//     }
+//   })
+// })
+
+// app.delete('/student', (req, res) => {
+//   const queryString = 'DELETE FROM students WHERE id= $1 RETURNING id'
+//   const values = [req.body.id]
+//   client.query(queryString, values, (err, response) => {
+//     if (err) throw err
+//     else {
+//       res.send(response.rows[0])
+//     }
+//   })
+// })
+
+// app.put('/student', (req, res) => {
+//   const queryString = 'UPDATE students SET name=$2 WHERE id=$1 RETURNING id'
+//   const values = [req.body.id, req.body.name]
+//   client.query(queryString, values, (err, response) => {
+//     if (err) throw err
+//     else {
+//       res.send(response.rows[0])
+//     }
+//   })
+// })
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
